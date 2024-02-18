4cc31e738314d3b0dd4be5179d37e227407e5d2c
Eric Spencer <eric@spenc.org>
2024-02-18 17:11:50 -0500

    initial sgtreets web site

---
+++ web/StreetFinder/Program.cs
@@ -0,0 +1,18 @@
+namespace StreetFinder
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddRazorPages();
+            var app = builder.Build();
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.MapRazorPages();
+            app.Run();
+        }
+    }
+}
