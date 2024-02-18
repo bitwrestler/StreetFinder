a249640e81eea5e8d30203f82c00eeb0d4dbf391
Eric Spencer <eric@spenc.org>
2024-02-18 14:50:57 -0500

    streets data conversion

---
+++ README.md
@@ -0,0 +1,4 @@
+Virginia Street Finder
+Search for street names in Virginia
+
+Dataset from: https://vgin.vdem.virginia.gov/datasets/cd9bed71346d4476a0a08d3685cb36ae/about
---
+++ data/street_data_converter.py
@@ -0,0 +1,29 @@
+import sys
+import csv
+import json
+import datetime
+
+JURISDICTIONS = {1 #alexandraia city
+                 ,4 #farifax county
+                 ,5 #fairfax city
+                 }
+
+def isjurd(j1 : int, j2 : int) -> bool:
+    return j1 in JURISDICTIONS or j2 in JURISDICTIONS
+
+streetdistinct = set()
+streetarray=[]
+with open(sys.argv[1], 'r', encoding='utf-8', newline='') as reader:
+    creader = csv.DictReader(reader)
+    for arow in creader:
+        fn = arow['FULLNAME'].upper()
+        int(arow['L_JURISDICTION'])
+        if isjurd(int(arow['L_JURISDICTION']), int(arow['R_JURISDICTION'])) and (fn not in streetdistinct) :
+            tdict = {'zipcode_range' : (int(arow['L_POSTAL_AREA']), int(arow['R_POSTAL_AREA'])), 'name' : fn  }
+            streetarray.append(tdict)
+            streetdistinct.add(fn)
+
+output_struct = {'update_date' : datetime.datetime.now().isoformat(), 'data' : streetarray }
+
+with open('streets.json', 'w', encoding='utf-8') as writer:
+    json.dump(output_struct, writer)
