d34a52ef345d9d4935c3f115b542370c493b0be4
Eric Spencer <eric@spenc.org>
2024-02-22 17:44:22 -0500

    tooltips for results; jquery.ui library

--- web/StreetFinder/wwwroot/js/site.js
+++ web/StreetFinder/wwwroot/js/site.js
@@ -1,4 +1,11 @@
-﻿function street_searcher(min_search_pattern) {
+﻿function formatToolTip(streetObj) {
+    var z = streetObj.zipCodeRange[0];
+    if (streetObj.zipCodeRange.length > 1)
+        z = z + "-" + streetObj.zipCodeRange[1];
+    return "zipcode: " + z;
+}
+
+function street_searcher(min_search_pattern) {
     var pat = $('#searcher').val();
     if (pat.length >= min_search_pattern) {
         $('#results').empty();
@@ -7,7 +14,7 @@
                 $.each(
                     results, function (i, street) {
                         var num = i + 1;
-                        var item = `<li>${num}. <strong>${street.name}</strong></li>`;
+                        var item = `<li title="${formatToolTip(street)}">${num}. <strong>${street.name}</strong></li>`;
                         $('#results').append(item);
                 }
                 );
