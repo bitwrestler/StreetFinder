a321cfa513d3a748f8d5aa3cb7d5d2a0be70a636
Eric Spencer <eric@spenc.org>
2024-02-24 12:45:09 -0500

    change soundex code

--- web/StreetFinder/Code/SoundsLikeHandler.cs
+++ web/StreetFinder/Code/SoundsLikeHandler.cs
@@ -12,89 +12,138 @@ namespace StreetFinder.Code
     {
         public IEnumerable<string> PhoneticTokens(string phrase)
         {
-            return phrase.Split(" ").Select(st => Soundex(st));
+            return phrase.Split(" ").Select(st => husseinbeygiSoundex.Soundex(st));
         }
 
-        private static readonly Regex rule_12 = new Regex("[^a-z]", RegexOptions.IgnoreCase);
-        private static readonly Regex rule_3 = new Regex("[aeihouwy]", RegexOptions.IgnoreCase);
-        /// <summary>
-        /// 
-        ///     Code lifted from https://github.com/haddow64/PhoneticSearch
-        /// 
-        /// 
-        ///     This algorithm is a modified version of the Soundex algorithm that does not
-        ///     retain the leading letter and has different letter sets.
-        ///     Part of below function based on existing soundex implementation found here:
-        ///     http://www.techrepublic.com/blog/software-engineer/how-do-i-implement-the-soundex-function-in-c/#.
-        /// </summary>
-        /// <param name="inputNames">Called twice for user input and parsing of lines in .txt file</param>
-        private static string Soundex(string inputNames)
+        class husseinbeygiSoundex
         {
-            var result = new StringBuilder();
-            var previousNumber = "";
-
-            //var removeSpaces = inputNames.Replace(" ", "");
-
-            // 1.  All non-alphabetic characters are ignored
-            // 2.  Word case is not significant
-            // Rule one implemented using regular expressions
-            // Rule two implemented from RegexOptions.IgnoreCase
-            // Slower than the C# implementation of IsLetterOrDigit but more commonly used because its easier to understand and maintain
-            var onlyAlphabetic = rule_12.Replace(inputNames, "");
-
-            // 3.  After the first letter, any of the following letters are discarded: A, E, I, H, O, U, W, Y.
-            // Rule three stores the first letter in getFirstLetter
-            // Then uses regular expressions to remove the defined letters and rejoins the first letter
-            if (onlyAlphabetic.Length == 0)
-                return string.Empty;
-            var getFirstLetter = onlyAlphabetic.Substring(0, 1);
-
-            var removeLetters = rule_3.Replace(onlyAlphabetic.Remove(0, 1), "");
-            inputNames = getFirstLetter + removeLetters;
-
-            // 4.  The following sets of letters are considered equivalent
-            // A, E, I, O, U
-            // C, G, J, K, Q, S, X, Y, Z
-            // B, F, P, V, W
-            // D, T
-            // M, N
-            // All others have no equivalent
-            if (inputNames.Length > 0)
+            private static SoundexResourcesModel XSResources = new SoundexResourcesModel();
+ 
+            class CharacterCodes
             {
-                for (var i = 1; i < inputNames.Length; i++)
+                public CharacterCodes()
                 {
-                    var evaluateLetter = inputNames.Substring(i, 1).ToLower();
-
-                    string currentNumber;
-                    if ("aeiou".IndexOf(evaluateLetter, StringComparison.Ordinal) > -1)
-                        currentNumber = "1";
-                    else if ("cgjkqsxyz".IndexOf(evaluateLetter, StringComparison.Ordinal) > -1)
-                        currentNumber = "2";
-                    else if ("bfpvw".IndexOf(evaluateLetter, StringComparison.Ordinal) > -1)
-                        currentNumber = "3";
-                    else if ("dt".IndexOf(evaluateLetter, StringComparison.Ordinal) > -1)
-                        currentNumber = "4";
-                    else if ("mn".IndexOf(evaluateLetter, StringComparison.Ordinal) > -1)
-                        currentNumber = "5";
-                    else
-                        currentNumber = "6";
-
-                    // 5.  Any consecutive occurrences of equivalent letters (after discarding letters in step 3) 
-                    // are considered as a single occurrence
-                    if (currentNumber != previousNumber)
-                        result.Append(currentNumber);
-
-                    if (result.Length == 4) break;
-
-                    if (currentNumber != "")
-                        previousNumber = currentNumber;
+                    _1 = new();
+                    _2 = new();
+                    _3 = new();
+                    _4 = new();
+                    _5 = new();
+                    _6 = new();
                 }
+
+                public List<string> _1 { get; set; }
+                public List<string> _2 { get; set; }
+                public List<string> _3 { get; set; }
+                public List<string> _4 { get; set; }
+                public List<string> _5 { get; set; }
+                public List<string> _6 { get; set; }
             }
 
-            if (result.Length < 4)
-                result.Append(new string('0', 4 - result.Length));
+            class SoundexResourcesModel
+            {
+                public SoundexResourcesModel()
+                {
+                    CharacterCodes = new();
+                    Vowls = new();
+                    Maps = new();
 
-            return result.ToString().ToUpper();
+                        CharacterCodes._1 = "B/P/F/V".Split('/').ToList();
+                        CharacterCodes._2 = "C/G/J/K/Q/S/X/Z".Split('/').ToList();
+                        CharacterCodes._3 = "D/T".Split('/').ToList();
+                        CharacterCodes._4 = "L".Split('/').ToList();
+                        CharacterCodes._5 = "M/N".Split('/').ToList();
+                        CharacterCodes._6 = "R".Split('/').ToList();
+                        Vowls = "A/E/I/U/Y/O/H/W".Split('/').ToList();
+                    
+                }
+                public CharacterCodes CharacterCodes { get; set; }
+                public List<string> Vowls { get; set; }
+                public List<KeyValue> Maps { get; set; }
+
+            }
+
+            class KeyValue
+            {
+                public KeyValue(char key, char value)
+                {
+                    Key = key;
+                    Value = value;
+                }
+                public char Key { get; set; }
+                public char Value { get; set; }
+            }
+
+            public static string Soundex(string word)
+            {
+                if (string.IsNullOrWhiteSpace(word))
+                {
+                    return string.Empty;
+                }
+
+                word = word.ToUpper();
+
+                var fLetter = word[0];
+                word = RemoveTheFirstCharacter(word);
+                word = CleanUpTheVowlsCharacter(word);
+
+                var Characters = word.ToList();
+
+                var buffer = new StringBuilder(4);
+                buffer.Append(fLetter);
+
+                foreach (var item in Characters)
+                {
+                    int currCode = GetCharacterCode(item.ToString());
+                    buffer.Append(currCode);
+                    if (buffer.Length == 4)
+                        break;
+                }
+                AddZeroToEndIfNeeded(buffer);
+                return buffer.ToString();
+            }
+
+            private static void AddZeroToEndIfNeeded(StringBuilder buffer)
+            {
+                if (buffer.Length < 4)
+                    buffer.Append('0', (4 - buffer.Length));
+            }
+
+            private static string RemoveTheFirstCharacter(string word)
+            {
+                return word.Substring(1);
+            }
+
+            private static int GetCharacterCode(string Characters)
+            {
+                if (XSResources.CharacterCodes._1.FirstOrDefault(x => x == Characters) != null) { return 1; }
+                if (XSResources.CharacterCodes._2.FirstOrDefault(x => x == Characters) != null) { return 2; }
+                if (XSResources.CharacterCodes._3.FirstOrDefault(x => x == Characters) != null) { return 3; }
+                if (XSResources.CharacterCodes._4.FirstOrDefault(x => x == Characters) != null) { return 4; }
+                if (XSResources.CharacterCodes._5.FirstOrDefault(x => x == Characters) != null) { return 5; }
+                if (XSResources.CharacterCodes._6.FirstOrDefault(x => x == Characters) != null) { return 6; }
+
+                return 0;
+            }
+
+            private static string CleanUpTheVowlsCharacter(string word)
+            {
+
+                word = RemoveWhitespace(word);
+
+                foreach (var item in XSResources.Vowls)
+                {
+                    word = word.Replace(item, "");
+                }
+
+                return word;
+            }
+
+            private static string RemoveWhitespace(string input)
+            {
+                return new string(input.ToCharArray()
+                    .Where(c => !char.IsWhiteSpace(c))
+                    .ToArray());
+            }
         }
     }
 }
